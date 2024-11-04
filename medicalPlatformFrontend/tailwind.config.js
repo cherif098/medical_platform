c22786077fa0f230a168c6d17c6afa9e7f1562cc
cherif098 <douadicherif098@gmail.com>
2024-11-04 14:59:39 -0500

    add TopDoctors.jsx

--- medicalPlatformFrontend/tailwind.config.js
+++ medicalPlatformFrontend/tailwind.config.js
@@ -8,6 +8,9 @@ export default {
     extend: {
       colors: {
         'primary' : "#1741DA"
+    },
+    gridTemplateColumns:{
+      'auto':'repeat(auto-fill,minmax(200px,1fr))'
     }
   },
 },
