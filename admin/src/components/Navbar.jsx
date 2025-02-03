906538d71fd2a30265b91d4ba764624af59d8a14
LizaHamzi <OuizaHamzi@teccart.online>
2025-02-03 12:08:01 -0500

    updtate navbar
    Include nurse authentication in Navbar

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,10 +3,12 @@ import { assets } from "../assets/assets";
 import { AdminContext } from "../context/AdminContext";
 import { useNavigate } from "react-router-dom";
 import { DoctorContext } from "../context/DoctorContext";
+import { NurseContext } from "../context/NurseContext";
 
 const Navbar = () => {
   const { aToken, setAToken } = useContext(AdminContext);
   const { dToken, setDToken } = useContext(DoctorContext);
+  const { nToken, setNToken } = useContext(NurseContext);
 
   const navigate = useNavigate();
 
@@ -16,13 +18,16 @@ const Navbar = () => {
     aToken && localStorage.removeItem("aToken");
     dToken && setDToken("");
     dToken && localStorage.removeItem("dToken");
+    nToken && setNToken("");
+    nToken && localStorage.removeItem("nToken");
   };
   return (
     <div className="flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white">
       <div className="flex items-center gap-2 text-sx">
         <img className="w-30 sm:w-40 cursor-pointer" src={assets.logo} alt="" />
         <p className="border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600">
-          {aToken ? "Admin" : "Doctor"}
+        {aToken ? "Admin" : dToken ? "Doctor" : "Nurse" }
+
         </p>
       </div>
       <button
