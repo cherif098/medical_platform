4096426845203a796559349b04eeaf85997f20a4
LizaHamzi <OuizaHamzi@teccart.online>
2025-02-04 00:03:12 -0500

    Update ReportList.jsx
    Ajout d'un bouton pour modifier les signes vitaux d'un patient par l'infirmier

--- admin/src/components/ReportsList.jsx
+++ admin/src/components/ReportsList.jsx
@@ -279,7 +279,7 @@ const ReportsList = ({ reports, patientId, navigate, hideCreateButton, onDownloa
     console.log("ScrollTo saved:", sessionStorage.getItem("scrollTo"));
     navigate(`/view-report/${report.REPORT_ID}`);
   }}
-  className="text-blue-500 hover:text-blue-700 flex items-center gap-1"
+  className="text-green-500 hover:text-green-700 flex items-center gap-1"
   title="Add Notes"
 >
   <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -287,10 +287,28 @@ const ReportsList = ({ reports, patientId, navigate, hideCreateButton, onDownloa
   </svg>
   Add Notes
 </button>
+)}
 
-
-
-
+{userRole === "Nurse" && (
+  <button
+  onClick={() => {
+    sessionStorage.setItem("scrollTo", "VitalSigns");
+    console.log("ScrollTo saved:", sessionStorage.getItem("scrollTo"));
+    navigate(`/view-report/${report.REPORT_ID}`);
+  }}
+  className="text-blue-500 hover:text-blue-700 flex items-center gap-1"
+  title="Vital Signs"
+>
+<svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+    />
+  </svg>
+  Vital Signs
+</button>
 )}
 
 
