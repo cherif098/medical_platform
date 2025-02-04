26fd3080eeee529a10d1d11876bb70479c06487a
LizaHamzi <OuizaHamzi@teccart.online>
2025-02-03 20:02:58 -0500

    Add nurse routes
    Ajout des routes pour l'authentification et la gestion des infirmiers
    
    Co-Authored-By: KOCEILA DJABALLAH <89519445+ITNKOC@users.noreply.github.com>
    Co-Authored-By: cherif098 <175618243+cherif098@users.noreply.github.com>
    Co-Authored-By: Ouali OULD BRAHAM <152246525+ouali99@users.noreply.github.com>

---
+++ medicalPlatformBackend/routes/nurseRoute.js
@@ -0,0 +1,30 @@
+import express from "express";
+import multer from "multer";
+import {
+  nurseLogin,
+  nurseProfile,
+  updateNurseProfileController,
+  getPatientsByHospital,
+  getNurseNote, createNurseNote, modifyNurseNote, transcribeNurseNote,
+  deleteNurseNote,
+} from "../controllers/nurseController.js";
+import authNurse from "../middlewares/authNurse.js";
+const upload = multer({ dest: "uploads/" });
+
+const nurseRouter = express.Router();
+nurseRouter.post("/login", nurseLogin);
+nurseRouter.get("/profile", authNurse, nurseProfile);
+nurseRouter.post("/update-profile", authNurse, updateNurseProfileController);
+nurseRouter.get("/patients", authNurse, getPatientsByHospital);
+nurseRouter.get("/nurse-note/:reportId", authNurse, getNurseNote);
+nurseRouter.post("/nurse-note", authNurse, createNurseNote);
+nurseRouter.put("/update-note/:reportId", authNurse, modifyNurseNote);
+nurseRouter.post("/speech/transcribe", upload.single("file"), transcribeNurseNote);
+nurseRouter.delete('/delete-note/:reportId', authNurse, deleteNurseNote);
+
+
+
+
+
+
+export default nurseRouter;
