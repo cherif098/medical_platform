84b02730573e22501db8c6975c1e17132020aca4
LizaHamzi <OuizaHamzi@teccart.online>
2025-02-03 19:57:15 -0500

    Add nurse-specific routes in reportRoute.js
    Ajout de nouvelles routes pour les infirmiers dans reportRoute.js autorisations pour sécuriser les endpoints

--- medicalPlatformBackend/routes/reportRoute.js
+++ medicalPlatformBackend/routes/reportRoute.js
@@ -5,39 +5,42 @@ import {
   getMedicalReport,
   updateMedicalReport,
   deleteMedicalReport,
-  generateReportPDF,
   getDoctorPatients,
   getPatientMedicalReport,
-  generatePatientReportPDF,
+  downloadPDF,
   getPatientMedicalReports,
+  getReports,
+  getReportDetails,
+  fetchNurseNotes
 } from "../controllers/reportController.js";
 import authDoctor from "../middlewares/authDoctor.js";
 import authPatient from "../middlewares/authPatient.js";
+import authNurse from "../middlewares/authNurse.js";
 
 const reportRouter = express.Router();
 
 // Routes pour les patients (à placer AVANT les routes avec :reportId)
 reportRouter.get("/patient-reports", authPatient, getPatientMedicalReports);
-reportRouter.get(
-  "/patient-report/:reportId",
-  authPatient,
-  getPatientMedicalReport
-);
-reportRouter.get(
-  "/patient-report/:reportId/pdf",
-
-  generatePatientReportPDF
-);
+reportRouter.get("/patient-report/:reportId",authPatient,getPatientMedicalReport);
+reportRouter.get("/patient-report/:reportId/pdf",downloadPDF);
 
 // Routes pour les médecins
 reportRouter.get("/patients", authDoctor, getDoctorPatients);
-reportRouter.get("/patient/:patientId", authDoctor, getMedicalReports);
+reportRouter.get("/patient/:patientId",authDoctor, getMedicalReports);
 reportRouter.post("/", authDoctor, createMedicalReport);
 
 // Routes avec :reportId en dernier
 reportRouter.get("/:reportId", authDoctor, getMedicalReport);
 reportRouter.put("/:reportId", authDoctor, updateMedicalReport);
 reportRouter.delete("/:reportId", authDoctor, deleteMedicalReport);
-reportRouter.get("/:reportId/pdf", authDoctor, generateReportPDF);
+reportRouter.get("/:reportId/pdf", authDoctor, downloadPDF);
+reportRouter.get("/:reportId/nurse-notes", authDoctor ,fetchNurseNotes);
+
+// nurses
+reportRouter.get("/nurse/patients/:patientId/reports", authNurse, getReports);
+reportRouter.get("/nurse/:reportId", authNurse, getReportDetails);
+reportRouter.get("/nurse/:reportId/pdf", authNurse, downloadPDF);
+
+
 
 export default reportRouter;
